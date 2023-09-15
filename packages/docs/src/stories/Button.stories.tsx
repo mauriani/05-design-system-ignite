fc19fa00669eacf2b3d293d213370766b64e19b4
mauriani <maurianim@gmail.com>
2023-09-15 11:07:57 -0300

    feat: adicionando o nosso primeiro component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  // quando coloco aqui os demias botões vai ter o text de envia
+  args: {
+    children: 'Enviar',
+  },
+} as Meta
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
