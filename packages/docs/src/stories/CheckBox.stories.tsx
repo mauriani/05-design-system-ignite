83b55949567b6f62ee3a0cb7f6c746149b01c818
mauriani <maurianim@gmail.com>
2023-09-19 10:41:24 -0300

    Componentes do Design System - Componente: CheckBox

---
+++ packages/docs/src/stories/CheckBox.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, CheckBox, CheckBoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: CheckBox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '$2',
+            alignItems: 'center',
+          }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta
+
+export const Primary: StoryObj<CheckBoxProps> = {
+  args: {},
+}
---
+++ packages/react/src/components/CheckBox/index.tsx
@@ -0,0 +1,15 @@
+import { Check } from 'phosphor-react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps } from 'react'
+
+export type CheckBoxProps = ComponentProps<typeof CheckboxContainer>
+
+export function CheckBox(props: CheckBoxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
